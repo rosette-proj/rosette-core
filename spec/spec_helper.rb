29c80ea4831dd411a8dc7a943c934b229b444699
Cameron Dutro <camertron@gmail.com>
2014-09-01 09:01:18 -0700

    Rebuild all repo fixtures when test suite loads

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,12 +11,17 @@ require 'spec/helpers/test_extractor'
 RSpec.configure do |config|
   config.mock_with :rr
 
-  before(:all) do
-    fixture_path = File.join(File.expand_path('./', File.dirname(__FILE__)), 'spec/fixtures/repos')
-    FileUtils.rm_rf(File.join(fixture_path, 'bin/*'))
-    Dir.glob(File.join(fixture_path, 'lib/*.rb')).each do |fixture_script|
-      load fixture_script
-    end
+  # build all repo fixtures
+  fixture_path = File.join(File.expand_path('./', File.dirname(__FILE__)), 'fixtures/repos')
+  bin_dir = File.join(fixture_path, 'bin')
+  puts "Removing old fixtures at #{fixture_path}"
+  FileUtils.rm_rf(bin_dir)
+  FileUtils.mkdir_p(bin_dir)
+
+  Dir.glob(File.join(fixture_path, 'lib/*.rb')).each do |fixture_script|
+    STDOUT.write("Building repo fixture in #{fixture_script} ... ")
+    load fixture_script
+    puts 'done.'
   end
 end
 
