df303244348a105710002b5bc88997a61393a7a8
Matt Low <mlow@lumoslabs.com>
2014-10-13 14:01:45 -0700

    Create phrase status module

---
+++ lib/rosette/data_stores/phrase_status.rb
@@ -0,0 +1,11 @@
+module Rosette
+  module DataStores
+    module PhraseStatus
+
+      UNTRANSLATED = 'UNTRANSLATED'
+      PENDING = 'PENDING'
+      TRANSLATED = 'TRANSLATED'
+
+    end
+  end
+end
