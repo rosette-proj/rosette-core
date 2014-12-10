5d062c6e35880e59d50819699ce89395ed792f8a
Matt Low <mlow@lumoslabs.com>
2014-12-10 12:34:15 -0800

    Add HeadSnapshotFactory#take_snapshot method

--- lib/rosette/core/snapshots/head_snapshot_factory.rb
+++ lib/rosette/core/snapshots/head_snapshot_factory.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 
-# java_import 'org.eclipse.jgit.revwalk.RevWalk'
-# java_import 'org.eclipse.jgit.revwalk.filter.RevFilter'
+java_import 'org.eclipse.jgit.revwalk.RevWalk'
+java_import 'org.eclipse.jgit.revwalk.filter.RevFilter'
 # java_import 'org.eclipse.jgit.treewalk.TreeWalk'
 # java_import 'org.eclipse.jgit.treewalk.filter.AndTreeFilter'
 # java_import 'org.eclipse.jgit.treewalk.filter.OrTreeFilter'
@@ -13,15 +13,27 @@ module Rosette
   module Core
 
     class HeadSnapshotFactory
-      include SnapshotFilterable
 
       attr_reader :repo
 
       def set_repo(repo)
         @repo = repo
+        self
       end
 
       def take_snapshot
+        rev_walk = RevWalk.new(repo.jgit_repo)
+        repo.all_head_refs.each_with_object({}) do |ref, snapshot|
+          rev_walk.reset
+          rev_walk.markStart(repo.get_rev_commit(ref, rev_walk))
+          # rev_walk.setRevFilter(RevFilter::NO_MERGES)
+
+          snapshot[ref] = if rev_commit = rev_walk.next
+            rev_commit.getId.name
+          end
+
+        end
+
       end
     end
 
