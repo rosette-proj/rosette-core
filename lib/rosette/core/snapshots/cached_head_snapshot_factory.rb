656fa134bca992fa9b0f8a3a55e140a7d3e9ef3e
Matt Low <mlow@lumoslabs.com>
2014-12-10 14:19:29 -0800

    Add CachedHeadSnapshotFactory

---
+++ lib/rosette/core/snapshots/cached_head_snapshot_factory.rb
@@ -0,0 +1,39 @@
+# encoding: UTF-8
+
+require 'digest/sha1'
+
+module Rosette
+  module Core
+
+    class CachedHeadSnapshotFactory < HeadSnapshotFactory
+      attr_reader :cache
+
+      def initialize(cache)
+        @cache = cache
+      end
+
+      protected
+
+      def process_ref(rev_walk, ref)
+        cache_key = head_snapshot_cache_key(
+          repo_config.name,
+          repo.get_rev_commit(ref, rev_walk).getId.name
+        )
+
+        cache.fetch(cache_key) do
+          super
+        end
+      end
+
+      def head_snapshot_cache_key(repo_name, commit_id)
+        ['head_snapshots', repo_name, commit_id)
+      end
+
+      def head_snapshot_factory
+        Rosette::Core::HeadSnapshotFactory
+      end
+
+    end
+
+  end
+end
--- lib/rosette/core/snapshots/head_snapshot_factory.rb
+++ lib/rosette/core/snapshots/head_snapshot_factory.rb
@@ -14,27 +14,32 @@ module Rosette
 
     class HeadSnapshotFactory
 
-      attr_reader :repo
+      attr_reader :repo_config
 
-      def set_repo(repo)
-        @repo = repo
+      def set_repo_config(repo_config)
+        @repo_config = repo_config
         self
       end
 
       def take_snapshot
-        rev_walk = RevWalk.new(repo.jgit_repo)
-        repo.all_head_refs.each_with_object({}) do |ref, snapshot|
-          rev_walk.reset
-          rev_walk.markStart(repo.get_rev_commit(ref, rev_walk))
-          # rev_walk.setRevFilter(RevFilter::NO_MERGES)
-
-          snapshot[ref] = if rev_commit = rev_walk.next
-            rev_commit.getId.name
-          end
-
+        rev_walk = RevWalk.new(repo_config.jgit_repo)
+        repo_config.repo.all_head_refs.each_with_object({}) do |ref, snapshot|
+          snapshot[ref] = process_ref(rev_walk, ref)
         end
-
       end
+
+      protected
+
+      def process_ref(rev_walk, ref)
+        rev_walk.reset
+        rev_walk.markStart(repo_config.repo.get_rev_commit(ref, rev_walk))
+        rev_walk.setRevFilter(RevFilter::NO_MERGES)
+
+        if rev_commit = rev_walk.next
+          rev_commit.getId.name
+        end
+      end
+
     end
 
   end
