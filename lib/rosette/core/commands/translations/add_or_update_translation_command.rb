f0200f9cdd4354b0ebbcd5595851d2fd88b4d783
Cameron Dutro <camertron@gmail.com>
2014-12-10 15:23:31 -0800

    Log the commit_id and locale when sending warnings to rollbar in AddOrUpdateTranslationCommand

--- lib/rosette/core/commands/translations/add_or_update_translation_command.rb
+++ lib/rosette/core/commands/translations/add_or_update_translation_command.rb
@@ -37,7 +37,7 @@ module Rosette
             }
           )
         rescue Rosette::DataStores::Errors::PhraseNotFoundError => e
-          configuration.error_reporter.report_warning(e)
+          configuration.error_reporter.report_warning(e, commit_id: commit_id, locale: locale)
         end
       end
 
