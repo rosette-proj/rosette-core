cef8ff39086e8593cf0f0871d96a2fab122eff34
Cameron Dutro <camertron@gmail.com>
2014-12-12 11:34:04 -0800

    Fix concurrency bug in WithRef
    Getting RevCommits can’t be done in parallel since #rev_commit uses a
    shared instance of Repo.

--- lib/rosette/core/commands/git/with_ref.rb
+++ lib/rosette/core/commands/git/with_ref.rb
@@ -1,11 +1,15 @@
 # encoding: UTF-8
 
+require 'thread'
+
 module Rosette
   module Core
     module Commands
 
       module WithRef
         attr_reader :commit_str
+
+        REV_COMMIT_MUTEX = Mutex.new
 
         def self.included(base)
           if base.respond_to?(:validate)
@@ -24,12 +28,13 @@ module Rosette
         end
 
         def commit_id
-          @commit_id ||= rev_commit.getId.name
-        end
-
-        def rev_commit
-          @rev_commit ||= get_repo(repo_name)
-            .repo.get_rev_commit(@commit_str)
+          @commit_id ||= begin
+            REV_COMMIT_MUTEX.synchronize do
+              get_repo(repo_name)
+                .repo.get_rev_commit(@commit_str)
+                .getId.name
+            end
+          end
         end
       end
 
