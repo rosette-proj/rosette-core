d365ebd6dcfccafa1de04f56b086f638904f051f
Cameron Dutro <camertron@gmail.com>
2014-12-26 21:43:11 -0800

    Put quotes around the repo name in the RepoValidator's error message

--- lib/rosette/core/validators/repo_validator.rb
+++ lib/rosette/core/validators/repo_validator.rb
@@ -7,7 +7,7 @@ module Rosette
       class RepoValidator < Validator
         def valid?(repo_name, _, configuration)
           if !configuration.get_repo(repo_name)
-            messages << "Unable to find repo #{repo_name}."
+            messages << "Unable to find repo '#{repo_name}'."
             false
           else
             true
