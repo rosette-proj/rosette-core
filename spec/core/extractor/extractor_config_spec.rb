a27a9b2fe3266146af1b94f41a9d72cb6532b84c
Cameron Dutro <camertron@gmail.com>
2015-01-09 16:40:35 -0800

    Fixing specs

--- spec/core/extractor/extractor_config_spec.rb
+++ spec/core/extractor/extractor_config_spec.rb
@@ -6,21 +6,23 @@ include Rosette::Core
 
 describe ExtractorConfig do
   let(:extractor_class) { Rosette::Extractors::Test::TestExtractor }
+  let(:extractor_id) { 'test/test' }
 
   it 'instantiates the extractor given' do
-    ExtractorConfig.new(extractor_class).tap do |config|
+    ExtractorConfig.new(extractor_id, extractor_class).tap do |config|
       expect(config.extractor).to be_instance_of(extractor_class)
+      expect(config.extractor_id).to eq(extractor_id)
     end
   end
 
   it 'sets a default encoding' do
-    ExtractorConfig.new(extractor_class).tap do |config|
+    ExtractorConfig.new(extractor_id, extractor_class).tap do |config|
       expect(config.encoding).to eq(Rosette::Core::DEFAULT_ENCODING)
     end
   end
 
   describe '#set_encoding' do
-    let(:config) { ExtractorConfig.new(extractor_class) }
+    let(:config) { ExtractorConfig.new(extractor_id, extractor_class) }
 
     it 'sets the encoding on the instance' do
       config.set_encoding(Encoding::UTF_16BE).tap do |config_with_encoding|
