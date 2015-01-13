e7c67cdb18abe53373abb0126267c7888874ef25
Cameron Dutro <camertron@gmail.com>
2015-01-12 22:44:58 -0800

    Add #set_paths to SnapshotCommand

--- lib/rosette/core/commands/git/snapshot_command.rb
+++ lib/rosette/core/commands/git/snapshot_command.rb
@@ -15,17 +15,32 @@ module Rosette
       #
       #   cmd.execute
       #   # => [<Phrase #ba012cd key: 'foobar'>, ...]
+      #
+      # @!attribute [Array] paths
+      #   @return [Array] the paths to consider when taking the snapshot. Any paths
+      #     that do not exist in this array will not appear in the snapshot.
       class SnapshotCommand < GitCommand
+        attr_reader :paths
+
         include WithSnapshots
         include WithRepoName
         include WithRef
+
+        # Set the paths that will be included in the snapshot.
+        #
+        # @param [Array] paths The paths to include in the snapshot.
+        # @return [self]
+        def set_paths(paths)
+          @paths = paths
+          self
+        end
 
         # Take the snapshot.
         #
         # @return [Array<Rosette::Core::Phrase>] the list of phrases from the
         #   snapshot.
         def execute
-          snapshot = take_snapshot(get_repo(repo_name), commit_id)
+          snapshot = take_snapshot(get_repo(repo_name), commit_id, paths)
           datastore.phrases_by_commits(repo_name, snapshot).to_a
         end
       end
