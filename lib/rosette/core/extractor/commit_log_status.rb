c5c2a0cfc76e88e148650bb3a4de878a385e4b73
Cameron Dutro <camertron@gmail.com>
2015-02-25 16:15:02 -0800

    Introducing some fancy metaprogramming so classes that include CommitLogStatus have their constructors called in the right order

--- lib/rosette/core/extractor/commit_log_status.rb
+++ lib/rosette/core/extractor/commit_log_status.rb
@@ -5,11 +5,15 @@ require 'state_machine'
 module Rosette
   module Core
 
+    # Provides a state machine for transitioning between the possible states of
+    # a commit log.
     module CommitLogStatus
       include Rosette::DataStores::PhraseStatus
 
       def self.included(base)
         base.class_eval do
+          class_initialize = instance_method(:initialize)
+
           state_machine :status, initial: UNTRANSLATED do
             # called on every push
             event :push do
@@ -38,6 +42,21 @@ module Rosette
             event :missing do
               transition all => MISSING
             end
+          end
+
+          state_machine_initialize = instance_method(:initialize)
+
+          # state_machine overrides base's initializer, which can be bad.
+          # For example, if base's initializer sets up the initial state of
+          # the state machine (eg. @state = 'foo'), the fact that the state
+          # machine's initializer gets called first means @state doesn't get
+          # set and therefore isn't available to the state machine (the
+          # initial state will be nil). By grabbing references to both
+          # initialize methods, we can re-define initialize to call them in
+          # the right order.
+          define_method(:initialize) do |*args, &block|
+            class_initialize.bind(self).call(*args, &block)
+            state_machine_initialize.bind(self).call(*args, &block)
           end
         end
       end
