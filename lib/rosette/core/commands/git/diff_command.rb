e76ed6d417140460f55ab662c1e728ec1f698110
Cameron Dutro <camertron@gmail.com>
2015-03-18 13:15:55 -0700

    Ok, I was wrong, don't use snapshots, but filter at the end (diff command)

--- lib/rosette/core/commands/git/diff_command.rb
+++ lib/rosette/core/commands/git/diff_command.rb
@@ -165,34 +165,43 @@ module Rosette
         #   contains the previous key of the phrase. See the example above for a visual
         #   representation of the diff hash.
         def execute
+          ensure_commits_have_been_processed([head_commit_id, diff_point_commit_id])
           repo_config = get_repo(repo_name)
+          rev_walker = RevWalk.new(repo_config.repo.jgit_repo)
+          diff_finder = DiffFinder.new(repo_config.repo.jgit_repo, rev_walker)
 
-          head_snapshot = filter_paths(
-            take_snapshot(repo_config, head_commit_id, paths), repo_config
-          )
+          repo = repo_config.repo
+          diff = repo.diff(head_commit_id, diff_point_commit_id, [], diff_finder)
+          head = repo.get_rev_commit(head_commit_id, rev_walker)
 
-          ensure_commits_have_been_processed(head_snapshot.values)
-          head_phrases = datastore.phrases_by_commits(repo_name, head_snapshot)
+          head_snapshot = {}
+          head_paths = []
 
-          diff_point_snapshot = filter_paths(
-            take_snapshot(repo_config, diff_point_commit_id, paths), repo_config
-          )
+          diff.each do |diff_entry|
+            path = diff_entry.getNewPath
+            if repo_config.extractor_configs.any? { |ext| ext.matches?(path) }
+              head_snapshot[path] = head_commit_id
+              head_paths << (path == '/dev/null' ? diff_entry.getOldPath : path)
+            end
+          end
 
+          head_phrases = datastore.phrases_by_commits(repo_name, head_snapshot).to_a
+          diff_point_snapshot = take_snapshot(repo_config, diff_point_commit_id, head_paths)
           ensure_commits_have_been_processed(diff_point_snapshot.values)
-          diff_point_phrases = datastore.phrases_by_commits(repo_name, diff_point_snapshot)
+          diff_point_phrases = datastore.phrases_by_commits(repo_name, diff_point_snapshot).to_a
 
-          compare(head_phrases, diff_point_phrases)
-        end
+          diff = compare(head_phrases, diff_point_phrases)
+          commit_id_enum = repo_config.repo.each_commit_in_range(head_commit_id, diff_point_commit_id)
+          commit_ids = commit_id_enum.map { |c| c.getId.name }
 
-        private
-
-        def filter_paths(snapshot, repo_config)
-          snapshot.each_with_object({}) do |(path, commit_id), ret|
-            if repo_config.extractor_configs.any? { |ex| ex.matches?(path) }
-              ret[path] = commit_id
+          diff.each_with_object({}) do |(state, diff_entries), ret|
+            ret[state] = diff_entries.select do |diff_entry|
+              commit_ids.include?(diff_entry.phrase.commit_id)
             end
           end
         end
+
+        private
 
         def cache_key
           [
