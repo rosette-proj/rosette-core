a1cdd6fc7f9d9b092081ace1fdf316e8ac238229
Cameron Dutro <camertron@gmail.com>
2015-05-21 15:32:14 -0700

    Adding delay to job instances

--- spec/queuing/job_spec.rb
+++ spec/queuing/job_spec.rb
@@ -17,4 +17,17 @@ describe Job do
       expect(job_class.queue_name).to eq('foobar')
     end
   end
+
+  describe '#set_delay' do
+    let(:job) { job_class.new }
+
+    it 'checks the delay is set to zero by default' do
+      expect(job.delay).to eq(0)
+    end
+
+    it 'sets the delay' do
+      job.set_delay(10)
+      expect(job.delay).to eq(10)
+    end
+  end
 end
